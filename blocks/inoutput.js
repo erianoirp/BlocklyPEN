86434f0b0a4886522976a999f9d20f1a6a06749d
omotonama <prilt5ogy@gmail.com>
2018-11-28 16:54:10 +0900

    コメントアウトされてるとこを削除

--- blocks/inoutput.js
+++ blocks/inoutput.js
@@ -1,20 +1,3 @@
-/*
-Blockly.Blocks['input'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("キーボードから、変数");
-    this.appendValueInput("val")
-        .setCheck(null);
-    this.appendDummyInput()
-        .appendField("に入力");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
-    this.setTooltip('');
-  }
-};
-*/
-
 Blockly.Blocks['input'] = {
   init: function() {
     this.appendValueInput("variable")
@@ -61,58 +44,3 @@ Blockly.Blocks['print_v'] = {
   }
 };
 
-Blockly.Blocks['output'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("コンソールに")
-        .appendField(new Blockly.FieldTextInput("≪出力文≫"), "val")
-        .appendField("を出力");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
-    this.setTooltip('');
-  }
-};
-
-Blockly.Blocks['outputno'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("コンソールに")
-        .appendField(new Blockly.FieldTextInput("≪出力文≫"), "val")
-        .appendField("を改行なしで出力");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
-    this.setTooltip('');
-  }
-};
-
-Blockly.Blocks['valoutput'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("コンソールに");
-    this.appendValueInput("val")
-        .setCheck(null);
-    this.appendDummyInput()
-        .appendField("を出力");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
-    this.setTooltip('');
-  }
-};
-
-Blockly.Blocks['valoutputno'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("コンソールに");
-    this.appendValueInput("val")
-        .setCheck(null);
-    this.appendDummyInput()
-        .appendField("を改行なしで出力");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
-    this.setTooltip('');
-  }
-};
