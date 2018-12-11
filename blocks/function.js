665d1936d853060ea8fdfd2bb8e33abe2f1d6225
omotonama <prilt5ogy@gmail.com>
2018-12-11 17:10:27 +0900

    関数ブロックの一時保存 引数の取り出しとmutatorを使うときにブロックが引きはがされるのを修正するのが次の課題

--- blocks/function.js
+++ blocks/function.js
@@ -1,3 +1,230 @@
+goog.provide('Blockly.Blocks.logic');
+goog.provide('Blockly.Constants.Logic');
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly');
+
+Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
+  {
+    "type": "define_function",
+    "message0": "関数 %1 %2 (",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "return_value",
+        "options": [["整数", "整数"], ["実数", "実数"], ["文字列", "文字列"], ["真偽", "真偽"]]
+      },
+      {
+        "type": "field_input",
+        "name": "func_name",
+        "text": "≪関数≫"
+      }
+    ],
+    "message1": "%1",
+    "args1": [
+      {
+        "type": "input_dummy",
+        "name": "PARAM0"
+      }
+    ],
+    "message2": ") %1",
+    "args2": [
+      {
+        "type": "input_dummy",
+        "name": "close_parenthesis"
+      }
+    ],
+    "message3": "%1",
+    "args3": [
+      {
+        "type": "input_statement",
+        "name": "STATEMENTS"
+      }
+    ],
+    "message4": "関数終了 %1",
+    "args4": [
+      {
+        "type": "input_dummy",
+        "name": "define_end"
+      }
+    ],
+    "inputsInline": true,
+    "colour": 160,
+    "tooltip": "",
+    "helpUrl": "",
+    "mutator": "define_function_mutator"
+  }
+]);
+
+Blockly.defineBlocksWithJsonArray([ // Mutator blocks. Do not extract.
+  /*
+  {
+    "type": "parameters",
+    "message0": "( %1",
+    "args0": [
+      {
+        "type": "input_dummy"
+      }
+    ],
+    "message1": "%1 )",
+    "args1": [
+      {
+        "type": "input_statement",
+        "name": "parameters"
+      }
+    ],
+    "enableContextMenu": false,
+    "colour": 160,
+    "tooltip": ""
+  },
+  */
+  {
+    "type": "parameters",
+    "message0": "仮引数たち",
+    "nextStatement": null,
+    "enableContextMenu": false,
+    "colour": 160,
+    "tooltip": ""
+  },
+  {
+    "type": "parameter",
+    "message0": "仮引数",
+    "previousStatement": null,
+    "nextStatement": null,
+    "enableContextMenu": false,
+    "colour": 160,
+    "tooltip": ""
+  }
+]);
+
+Blockly.Constants.Logic.DEFINE_FUNCTION_MUTATOR_MIXIN = {
+  parameterCount: 0,
+
+  /**
+   * Create XML to represent the number of else-if and else inputs.
+   * @return {Element} XML storage element.
+   * @this Blockly.Block
+   */
+  mutationToDom: function() {
+    if (!this.parameterCount) {
+      return null;
+    }
+    var container = document.createElement('mutation');
+    container.setAttribute('parameter', this.parameterCount);
+    return container;
+  },
+  /**
+   * Parse XML to restore the else-if and else inputs.
+   * @param {!Element} xmlElement XML storage element.
+   * @this Blockly.Block
+   */
+  domToMutation: function(xmlElement) {
+    this.parameterCount = parseInt(xmlElement.getAttribute('parameter'), 10) || 0;
+    this.updateShape_();
+  },
+  /**
+   * Populate the mutator's dialog with this block's components.
+   * @param {!Blockly.Workspace} workspace Mutator's workspace.
+   * @return {!Blockly.Block} Root block in mutator.
+   * @this Blockly.Block
+   */
+  decompose: function(workspace) {
+    var containerBlock = workspace.newBlock('parameters');
+    containerBlock.initSvg();
+    var connection = containerBlock.nextConnection;
+    for (var i = 1; i <= this.parameterCount; i++) {
+      var parameterBlock = workspace.newBlock('parameter');
+      parameterBlock.initSvg();
+      connection.connect(parameterBlock.previousConnection);
+      connection = parameterBlock.nextConnection;
+    }
+    return containerBlock;
+  },
+  /**
+   * Reconfigure this block based on the mutator dialog's components.
+   * @param {!Blockly.Block} containerBlock Root block in mutator.
+   * @this Blockly.Block
+   */
+  compose: function(containerBlock) {
+    var clauseBlock = containerBlock.nextConnection.targetBlock();
+    this.parameterCount = 0;
+    var statementConnection = containerBlock.statementConnection_;
+    while (clauseBlock) {
+      switch (clauseBlock.type) {
+        case 'parameter':
+          this.parameterCount++;
+          break;
+        default:
+          throw 'Unknown block type.';
+      }
+      clauseBlock = clauseBlock.nextConnection &&
+          clauseBlock.nextConnection.targetBlock();
+    }
+    this.updateShape_();
+    Blockly.Mutator.reconnect(statementConnection, this, 'STATEMENTS');
+  },
+  /**
+   * Store pointers to any connected child blocks.
+   * @param {!Blockly.Block} containerBlock Root block in mutator.
+   * @this Blockly.Block
+   */
+  saveConnections: function(containerBlock) {
+    var clauseBlock = containerBlock.nextConnection.targetBlock();
+    var i = 1;
+    while (clauseBlock) {
+      switch (clauseBlock.type) {
+        case 'parameter':
+          var inputParam = this.getInput('PARAM' + i);
+/*
+          clauseBlock.valueConnection_ =
+              inputParam && inputParam.connection.targetConnection;
+*/
+          i++;
+          break;
+        default:
+          throw 'Unknown block type.';
+      }
+      clauseBlock = clauseBlock.nextConnection &&
+          clauseBlock.nextConnection.targetBlock();
+    }
+  },
+  /**
+   * Modify this block to have the correct number of inputs.
+   * @this Blockly.Block
+   * @private
+   */
+  updateShape_: function() {
+    // Delete until parameter part.
+    this.removeInput('define_end');
+    this.removeInput('STATEMENTS');
+    this.removeInput('close_parenthesis');
+    var i = 1;
+    while (this.getInput('PARAM' + i)) {
+      this.removeInput('PARAM' + i);
+      i++;
+    }
+    // Rebuild block.
+    for (var i = 1; i <= this.parameterCount; i++) {
+      this.appendDummyInput('PARAM' + i)
+          .appendField(new Blockly.FieldDropdown([["整数","整数"], ["実数","実数"], ["文字列","文字列"], ["真偽","真偽"]]), "DATATYPE" + i)
+          .appendField(new Blockly.FieldTextInput("≪仮引数≫"), "NAME" + i);
+    }
+    this.appendDummyInput('close_parenthesis')
+        .appendField(')');
+    this.appendStatementInput('STATEMENTS');
+    this.appendDummyInput('define_end')
+        .appendField('関数終了');
+  }
+};
+
+Blockly.Extensions.registerMutator(
+  'define_function_mutator',
+  Blockly.Constants.Logic.DEFINE_FUNCTION_MUTATOR_MIXIN,
+  null,
+  ['parameter']
+);
+
+/*
 Blockly.Blocks['define_function'] = {
   init: function() {
     this.appendValueInput("parameters")
@@ -16,6 +243,7 @@ Blockly.Blocks['define_function'] = {
  this.setHelpUrl("");
   }
 };
+*/
 
 Blockly.Blocks['define_step'] = {
   init: function() {
@@ -59,6 +287,7 @@ Blockly.Blocks['return_void_block'] = {
   }
 };
 
+/*
 Blockly.Blocks['parameter'] = {
   init: function() {
     this.appendValueInput("parameter")
@@ -71,6 +300,7 @@ Blockly.Blocks['parameter'] = {
  this.setHelpUrl("");
   }
 };
+*/
 
 Blockly.Blocks['call_step'] = {
   init: function() {
