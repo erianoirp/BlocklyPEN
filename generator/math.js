06a8fea8cde67609e9b8af5f52684a155f9b5f99
omotonama <prilt5ogy@gmail.com>
2018-12-14 15:00:43 +0900

    細かな修正

--- generator/math.js
+++ generator/math.js
@@ -1,29 +1,16 @@
 //WaPENで使用できる
 Blockly.Pen['calculation'] = function(block) {
-  var value_val1 = Blockly.Pen.valueToCode(block, 'val1', Blockly.Pen.ORDER_ATOMIC);
-  var dropdown_calculation = block.getFieldValue('calculation');
-  var value_val2 = Blockly.Pen.valueToCode(block, 'val2', Blockly.Pen.ORDER_ATOMIC);
-  // 括弧を取り除く
-  value_val1 = value_val1.substr(1);
-  value_val1 = value_val1.substr(0,value_val1.length-1);
-  value_val2 = value_val2.substr(1);
-  value_val2 = value_val2.substr(0,value_val2.length-1);
-  // TODO: Assemble Pen into code variable.
-  var code = value_val1 + ' ' +  dropdown_calculation + ' ' + value_val2;
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Pen.ORDER_NONE];
-  // return code;
+  var val1 = Blockly.Pen.valueToCode(block, 'val1', Blockly.Pen.ORDER_ATOMIC);
+  var calculation = block.getFieldValue('calculation');
+  var val2 = Blockly.Pen.valueToCode(block, 'val2', Blockly.Pen.ORDER_ATOMIC);
+  var code = val1 + ' ' +  calculation + ' ' + val2;
+  return [code, Blockly.Pen.ORDER_ATOMIC];
 };
 
 Blockly.Pen['random'] = function(block) {
-  var value_val = Blockly.Pen.valueToCode(block, 'val', Blockly.Pen.ORDER_ATOMIC);
-  // 括弧を取り除く
-  value_val = value_val.substr(1);
-  value_val = value_val.substr(0,value_val.length-1);
-  // TODO: Assemble Pen into code variable.
-  var code = 'random(' + value_val +  ')';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Pen.ORDER_NONE];
+  var val = Blockly.Pen.valueToCode(block, 'val', Blockly.Pen.ORDER_ATOMIC);
+  var code = 'random(' + val +  ')';
+  return [code, Blockly.Pen.ORDER_ATOMIC];
 };
 
 //-------------------------------------------------------------------------------------
