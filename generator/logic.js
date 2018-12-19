df7686ba79be70a7372532d81c53263f380782e4
erianoirp <prilt5ogy@gmail.com>
2018-12-19 13:36:48 +0900

    比較ブロックを修正

--- generator/logic.js
+++ generator/logic.js
@@ -3,14 +3,7 @@ Blockly.Pen['conditions'] = function(block) {
   var value_argument1 = Blockly.Pen.valueToCode(block, 'argument1', Blockly.Pen.ORDER_ATOMIC);
   var dropdown_comparisonoperator = block.getFieldValue('comparisonOperator');
   var value_argument2 = Blockly.Pen.valueToCode(block, 'argument2', Blockly.Pen.ORDER_ATOMIC);
-  // 括弧を取り除く
-  value_argument1 = value_argument1.substr(1);
-  value_argument1 = value_argument1.substr(0,value_argument1.length-1);
-  value_argument2 = value_argument2.substr(1);
-  value_argument2 = value_argument2.substr(0,value_argument2.length-1);
-  // TODO: Assemble Pen into code variable.
   var code = value_argument1 + " " + dropdown_comparisonoperator + " " + value_argument2;
-  // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Pen.ORDER_NONE];
 };
 
