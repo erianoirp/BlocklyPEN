8e5650e9030ba00cc04595d254aada379d448491
erianoirp <prilt5ogy@gmail.com>
2018-12-19 14:28:45 +0900

    配列ブロックを追加

--- blocks/variable.js
+++ blocks/variable.js
@@ -32,6 +32,39 @@ Blockly.Blocks['declare_variable'] = {
   }
 };
 
+Blockly.Blocks['declare_array'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 %2 大きさ %3 の配列を宣言",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "datatype",
+          "options": [["整数","整数"],["実数","実数"],["文字列","文字列"],["真偽","真偽"]]
+        },
+        {
+          "type": "input_value",
+          "name": "NAME",
+          "check": "Variable"
+        },
+        {
+          "type": "field_number",
+          "name": "SIZE",
+          "value": 1,
+          "min": 1,
+          "precision": 1
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 330,
+      "tooltip": "",
+      "helpUrl": ""
+    });
+  }
+};
+
 Blockly.Blocks['assign'] = {
   init: function() {
     this.appendValueInput("to")
@@ -69,6 +102,30 @@ Blockly.Blocks['variable'] = {
     this.setColour(330);
  this.setTooltip("");
  this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['array'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 の %2 番目",
+      "args0": [
+        {
+          "type": "field_variable",
+          "name": "NAME",
+          "variable": "≪変数≫"
+        },
+        {
+          "type": "input_value",
+          "name": "INDEX"
+        }
+      ],
+      "inputsInline": true,
+      "output": null,
+      "colour": 330,
+      "tooltip": "",
+      "helpUrl": ""
+    });
   }
 };
 
--- generator/variable.js
+++ generator/variable.js
@@ -14,6 +14,14 @@ Blockly.Pen['declare_variable'] = function(block) {
   return code;
 };
 
+Blockly.Pen['declare_array'] = function(block) {
+  var datatype = block.getFieldValue('datatype');
+  var name = Blockly.Pen.valueToCode(block, 'NAME', Blockly.Pen.ORDER_ATOMIC);
+  var size = block.getFieldValue('SIZE');
+  var code = datatype + ' ' + name + '[' + size + ']\n';
+  return code;
+};
+
 Blockly.Pen['assign'] = function(block) {
   const to = Blockly.Pen.valueToCode(block, 'to', Blockly.Pen.ORDER_ATOMIC);
   const from = Blockly.Pen.valueToCode(block, 'from', Blockly.Pen.ORDER_ATOMIC);
@@ -32,5 +40,12 @@ Blockly.Pen['variable'] = function(block) {
 Blockly.Pen['variable'] = function(block) {
   const name = Blockly.Pen.variableDB_.getName(block.getFieldValue('name'), Blockly.Variables.NAME_TYPE);
   const code = name;
+  return [code, Blockly.Pen.ORDER_ATOMIC];
+};
+
+Blockly.Pen['array'] = function(block) {
+  var name = Blockly.Pen.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
+  var index = Blockly.Pen.valueToCode(block, 'INDEX', Blockly.Pen.ORDER_ATOMIC);
+  var code = name + '[' + index + ']';
   return [code, Blockly.Pen.ORDER_ATOMIC];
 };
